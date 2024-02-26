17929ec45b75f9053de016a02254afdc72838921
Hasina235 <hasinaandria235@gmail.com>
2024-02-26 20:03:12 +0300

    change

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const mysql = require('mysql');
+const db = mysql.createConnection({
+    host: "localhost", user: "root", password: ""
+});
 
+
+app.get('/', (req, res)=> {
+    db.connect(function(err){
+        if(err) throw err;
+        console.log("connecté à la base de donnée");
+    });
+});
 app.listen(PORT, console.log('serveur prêt a ecouter au port'+PORT));
